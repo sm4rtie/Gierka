2ffda6f41bc06d76cb0d204309cb248b03177615
sm4rtie <marta.jareckaa@gmail.com>
2017-05-10 00:31:10 +0200

    ...

--- app/src/main/java/com/example/maj/gierka/MainActivity.java
+++ app/src/main/java/com/example/maj/gierka/MainActivity.java
@@ -1,16 +1,7 @@
 package com.example.maj.gierka;
 
-import android.Manifest;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.BroadcastReceiver;
-import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
-import android.content.pm.PackageManager;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
@@ -22,42 +13,34 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.ImageButton;
 import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
-    private static final int REQUEST_ENABLE_BT = 1;
-    private static final int REQ_BT_ENABLE = 1;
-    Button practiceBtn;
-    Button playBtn;
+    ImageButton practiceBtn;
+    ImageButton playBtn;
     Button settingsBtn;
     PopupWindow popupWindow;
     LayoutInflater layoutInflater;
     RelativeLayout relativeLayout;
     CheckBox soundCheck;
 
-    BluetoothAdapter mBluetoothAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
         //fullscreen
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         this.setContentView(R.layout.activity_main);
 
-
-
         relativeLayout = (RelativeLayout) findViewById(R.id.relative);
-        practiceBtn = (Button) findViewById(R.id.practiceBtn);
-        playBtn = (Button) findViewById(R.id.playBtn);
-        soundCheck = (CheckBox) findViewById(R.id.soundCheck);
+        practiceBtn = (ImageButton) findViewById(R.id.practiceBtn);
+        playBtn = (ImageButton) findViewById(R.id.playBtn);
+        //soundCheck = (CheckBox) findViewById(R.id.soundCheck);
        // settingsBtn = (Button) findViewById(R.id.setBtn);
        /* settingsBtn.setOnClickListener(new View.OnClickListener() {
     @Override
@@ -76,31 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
     }
         });*/
-
     }
-
-  /**  private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                // Discovery has found a device. Get the BluetoothDevice
-                // object and its info from the Intent.
-                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-            }
-        }
-    };
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        // Don't forget to unregister the ACTION_FOUND receiver.
-        unregisterReceiver(mReceiver);
-    }
-**/
 
     @Override
     public void onClick(View view) {
@@ -109,21 +68,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent i=new Intent(getApplicationContext(),BluetoothDiscovery.class);
         startActivity(i);
     }
-   /** @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-// TODO Auto-generated method stub
-        if(requestCode == REQ_BT_ENABLE){
-            if (resultCode == RESULT_OK){
-
-                Toast.makeText(getApplicationContext(), "BlueTooth is now Enabled", Toast.LENGTH_LONG).show();
-              //  Intent i=new Intent(getApplicationContext(),BluetoothDiscovery.class);
-               // startActivity(i);
-            }
-            if(resultCode == RESULT_CANCELED){
-                Toast.makeText(getApplicationContext(), "Error occured while enabling.Leaving the application..", Toast.LENGTH_LONG).show();
-                ///finish();
-            }
-        }
-    }**/
-
 }
